7df0dbea7f5fbadbc1d081c98eb081ef346d24aa
Raksha Raut <raksharaut174@gmail.com>
2024-06-05 15:22:17 +0545

    fix'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
+'use client';
+
 import React from 'react'
 
 import styles from './page.module.css'
 import CustomButton from '@/components/CustomButton'
+import { useRouter } from 'next/navigation'
 
 const HomePage = () => 
-    <div className={styles.main}>
+   {
+    const router = useRouter();
+    const nav=(text:string)=>{
+      router.push(text);
+    }
+    return ( <div className={styles.main}>
 
-      <CustomButton name='Sign in'/>
-      <CustomButton name='Dashboard'/>
-      
-    </div>
+    <CustomButton name='Sign in' onClick={(_e)=>{
+      nav('login');
+    }} />
+    <CustomButton name='Dashboard' onClick={(_e)=>{
+      nav('dashboard');
+    }}/>
+    
+  </div>)
+   }
 
 export default HomePage
